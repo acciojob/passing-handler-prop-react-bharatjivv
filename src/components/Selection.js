051e0bdb5b843978f1671de7acdec76832134ff6
BharatTate <hibharatj@gmail.com>
2025-07-19 12:36:57 +0530

    done

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React, { useState } from 'react';
 
+const Selection = ({ applyColor }) => {
+  const [bgStyle, setBgStyle] = useState({});
+
+  const handleClick = () => {
+    applyColor(setBgStyle);
+  };
+
+  return (
+    <div
+      className="fix-box"
+      onClick={handleClick}
+      style={bgStyle}
+    ></div>
+  );
+};
+
+export default Selection;
